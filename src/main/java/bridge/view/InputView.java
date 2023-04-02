c32098963f4a398c49b75abd8bdcec89683cf119
JunHyeongKim73 <rla5764v@gmail.com>
2023-04-02 23:17:08 +0900

    feat: 입력을 담당하는 클래스를 구현한다.

---
+++ src/main/java/bridge/view/InputView.java
@@ -0,0 +1,42 @@
+package bridge.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+/**
+ * 사용자로부터 입력을 받는 역할을 한다.
+ */
+public class InputView {
+
+    /**
+     * 다리의 길이를 입력받는다.
+     */
+    public int readBridgeSize() {
+        System.out.println("다리의 길이를 입력하세요.");
+        try {
+            final String input = Console.readLine();
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 다리의 길이는 숫자여야 합니다.");
+        }
+    }
+
+    /**
+     * 사용자가 이동할 칸을 입력받는다.
+     */
+    public String readMoving() {
+        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
+        return Console.readLine();
+    }
+
+    /**
+     * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
+     */
+    public String readGameCommand() {
+        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+        final String input = Console.readLine();
+        if(!"R".equals(input) && !"Q".equals(input)) {
+            throw new IllegalArgumentException("[ERROR] 잘못된 입력입니다.");
+        }
+        return input;
+    }
+}
